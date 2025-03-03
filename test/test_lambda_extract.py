e4050d902587ca6220e705383468c2a0c8824e9e
Armatoor147 <vincent.toor@gmail.com>
2025-03-03 16:33:43 +0100

    add test_lambda_extract.py

---
+++ test/test_lambda_extract.py
@@ -0,0 +1,12 @@
+from src.lambda_extract import get_secret, create_conn, close_db, get_rows_and_columns_from_table, write_table_to_s3, log_file, lambda_handler
+
+
+class TestGetSecret:
+    pass
+
+class GetSecret:
+    pass
+
+
+
+create_conn, close_db, get_rows_and_columns_from_table, write_table_to_s3, log_file, lambda_handler
