e9540d635e3390cf97bb0be71278e7a7a4aa24c6
Fabio Greenwood <fabio.greenwood@gmail.com>
2025-03-07 15:14:47 +0000

    re-added the branch changes to util file

--- src/util.py
+++ src/util.py
@@ -1,6 +1,5 @@
 import json
-from datetime import datetime
-
+from  datetime import datetime, date
 from botocore.exceptions import ClientError, NoCredentialsError
 import pandas as pd
 from pg8000.native import Connection
@@ -131,3 +130,33 @@ def json_to_pg8000_output(filepath, include_cols_in_output=True):
     f.close()
 
     return simulated_pg8000_output, simulated_pg8000_output_cols
+
+
+def return_week(date_isoformat_str):
+    date_object = date.fromisoformat(date_isoformat_str)
+    weekday_num = date_object.isoweekday()
+    weekday_name_dict = {1 : "monday", 2 : "tuesday", 3 : "wednesday", 4 : "thursday", 5 : "friday", 6 : "saturday", 7 : "sunday"}
+    weekday_name = weekday_name_dict[weekday_num]
+    return weekday_num, weekday_name    
+
+
+def return_s3_key(table_name, datetime_string, extension=".json"):
+    return f'data/{table_name}/{datetime_string}/{table_name}{extension}'
+
+def return_datetime_string():
+    timestamp = datetime.now()
+    year, month, day, hour, minute = timestamp.year, timestamp.month, timestamp.day, timestamp.hour, timestamp.minute
+    return f'{year}-{month}-{day}_{hour}-{minute}'
+
+
+def simple_read_parquet_file_into_dataframe(bucket_name, key, s3_client):
+    """source: https://stackoverflow.com/questions/51027645/how-to-read-a-single-parquet-file-in-s3-into-pandas-dataframe-using-boto3
+    """
+    # Read the parquet file
+    buffer = io.BytesIO()
+    object = s3_client.Object(bucket_name,key)
+    object.download_fileobj(buffer)
+    df = pd.read_parquet(buffer)
+    return df
+
+
