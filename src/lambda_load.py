4c828c5b4c74c1f0fcdcb00ec693707681475f23
Fabio Greenwood <fabio.greenwood@gmail.com>
2025-03-12 10:58:07 +0000

    this version works, the hardcoding needs to be removed

--- src/lambda_load.py
+++ src/lambda_load.py
@@ -10,20 +10,12 @@ from src.lambda_transform_utils import (return_s3_key)
 
 def load_connection():
     try:
-        region_name = "eu-west-2"
-        sm_client = boto3.client('secretsmanager', region_name=region_name)
-        get_secret_value_response = sm_client.get_secret_value(
-            SecretId="data-warehouse-credentials"
-        )
-
-        creds = get_secret_value_response['SecretString']
-        dw_access = json.loads(creds)
-
         conn = psycopg2.connect(
-            dbname=dw_access['database'], 
-            user=dw_access['user'], 
-            password=dw_access['password'], 
-            host=dw_access['host']
+            dbname = 'postgres',
+            user = 'project_team_6',
+            password = 'maSaxIhJnmv4bOk',
+            host = 'nc-data-eng-project-dw-prod.chpsczt8h1nu.eu-west-2.rds.amazonaws.com',
+            port=5432
             )
         return conn
     except Exception as e:
@@ -51,30 +43,32 @@ def lambda_handler(event, context):
         start_time = time.time()
         conn = load_connection()
         cursor = conn.cursor()
-         
-        bucket_name = 'totesys-test-processed'
-        datetime_string = event["datetime_string"]
+
+        bucket_name = 'totesys-processed-zone-fenor'
+        latest_folder = 'data/'
         s3_client = boto3.client("s3", region_name="eu-west-2")
-        s3_list = s3_client.list_objects_v2(Bucket=bucket_name)
-        s3_files = [file['Key'] for file in s3_list['Contents']]
+        #s3_list = s3_client.list_objects_v2(Bucket=bucket_name)
+        #s3_files = [file['Key'] for file in s3_list['Contents']]
+
+        list_of_tables = ["dim_date", "dim_design", "dim_location", "dim_counterparty", "dim_staff", "dim_currency", "fact_sales_order"]
+
         # Insert statement
-        for file in s3_files:
-            table_name = os.path.splitext(os.path.basename(file))[0]
-            if table_name == '':
-                continue
-            s3_key = return_s3_key("sales_order",    datetime_string)
-            
-            
+        for file in list_of_tables:
+            #print(f"s3_files: {s3_files}")
+            #table_name = os.path.splitext(os.path.basename(file))[0]
+            #s3_key = f'{latest_folder}{table_name}.parquet'
+            #if s3_key == 'data/.parquet':
+            #    continue
+            s3_key = return_s3_key(file, event["datetime_string"], extension=".parquet")
+            print(f"s3_key:{s3_key}")
             s3_response = s3_client.get_object(Bucket=bucket_name, Key=s3_key)
-            
             # Read parquet file
             parquet_data = s3_response["Body"].read()
             df = pd.read_parquet(io.BytesIO(parquet_data))
-
             df = df.reset_index()
 
             # Limit for time factor
-            if table_name == "fact_sales_order":
+            if file == "sales_order":
                 df = df.head(6000)
 
             # Build Insert query
@@ -82,13 +76,14 @@ def lambda_handler(event, context):
             df_columns = ", ".join(df_columns)
             id_column = df_columns.split(",")[0].strip()
             df_placeholders = ", ".join(["%s"] * len(df.columns))
+
             insert_query = f"""
-            INSERT INTO {table_name} 
+            INSERT INTO {file} 
             ({df_columns})
             VALUES 
             ({df_placeholders})
             """
-
+            print(f"insert_query:{insert_query}")
 
             for _, row in df.iterrows():
                 row_dict = row.to_dict()
@@ -104,4 +99,4 @@ def lambda_handler(event, context):
     except Exception as e:
         return {'message': f'Error: {e}'}
     
-print(lambda_handler({},{}))
+#print(lambda_handler({"datetime_string":"20250311_151013"},{}))
